3b506c2a233d3b073b4f31aab8791ae7eae17239
Mattias Holmlund <mattias@holmlund.se>
2019-09-09 08:10:40 +0200

    buildGraph

--- index.ts
+++ index.ts
@@ -4,19 +4,48 @@ export function gridGraphPlacement(nodes: GGNode[], connectors: GGConnector[]) {
     return [[{ id: 'A' }, { id: 'B' }, { id: 'C' }]]
 }
 
-function buildGraph(nodes: GGNode[], connectors: GGConnector[]) {
+export function buildGraph(nodes: GGNode[], connectors: GGConnector[]) {
+    const matrix: Matrix = []
     // All direct upstream nodes from one node
     const upstream = new Map<string, string[]>()
 
     // All direct downstream nodes from one node
     const downstream = new Map<string, string[]>()
 
+    // These nodeIds have already been placed on the matrix
+    const placed = new Set<string>()
+
     const nodeMap = new Map<string, GGNode>()
 
-    const result: Matrix = []
+    function placeNodeAndDownstreams(
+        node: GGNode,
+        row: number,
+        column: number,
+    ) {
+        if (!placed.has(node.id)) {
+            let freeRow = findFreeCell(matrix, row, column)
+            setCell(matrix, freeRow, column, node)
+            placed.add(node.id)
+            const downstreamNodes = (downstream.get(node.id) || [])
+                .filter(ds => !(downstream.get(ds) || []).includes(node.id))
+                .sort((a, b) => a.localeCompare(b))
+            for (let ds of downstreamNodes) {
+                const n = nodeMap.get(ds)
+                if (n) {
+                    placeNodeAndDownstreams(n, freeRow, column + 1)
+                }
+            }
+            const bidirDownstreamNodes = (downstream.get(node.id) || [])
+                .filter(ds => (downstream.get(ds) || []).includes(node.id))
+                .sort((a, b) => a.localeCompare(b))
 
-    function placeNodeAndDownstreams(node: GGNode, column: number) {
-        // setCell(result, column, findFreeCell(matrix, column), node)
+            for (let ds of bidirDownstreamNodes) {
+                const n = nodeMap.get(ds)
+                if (n) {
+                    placeNodeAndDownstreams(n, freeRow, column)
+                }
+            }
+        }
     }
 
     nodes.forEach(node => {
@@ -56,161 +85,11 @@ function buildGraph(nodes: GGNode[], connectors: GGConnector[]) {
             if (!node) {
                 throw new Error('Internal error')
             }
-            placeNodeAndDownstreams(node, 1)
+            placeNodeAndDownstreams(node, 0, 0)
         })
+
+    return matrix
 }
-
-// function placeDownstreamGroups(upstreamId, column) {
-//     nodesSorted.forEach(pg => {
-//         var upstreamGroups = (pg.upstreamGroups || '').split(',')
-//         if (upstreamGroups && upstreamGroups.indexOf(upstreamId) > -1) {
-//             if (!pitcherGroupPlaced[pg.id]) {
-//                 placeGroupInColumn(pg.id, column)
-//                 pitcherGroupPlaced[pg.id] = true
-//                 placeDownstreamGroups(pg.id, column + 1)
-//             } else if (nodesSorted[pg.id].x > column) {
-//                 freeUpRow(
-//                     indexes,
-//                     nodesSorted[pg.id].x,
-//                     nodesSorted[pg.id].y,
-//                 )
-//                 delete nodesSorted[pg.id]
-//                 placeGroupInColumn(pg.id, column)
-//                 pitcherGroupPlaced[pg.id] = true
-//                 placeDownstreamGroups(pg.id, column + 1)
-//             }
-//             connectors.push({ from: upstreamId, to: pg.id })
-//         }
-//     })
-// }
-
-// function placeGroupInColumn(pgid, column) {
-//     if (!nodesSorted[pgid]) {
-//         nodesSorted[pgid] = {
-//             x: column,
-//             y: firstFreeRow(indexes, column),
-//             purpose: pitcherCountString(pgid),
-//             name: pgid,
-//             class: 'downdown',
-//         }
-//     }
-// }
-
-// function pitcherCountString(pgid) {
-//     var c = pitcherCount[pgid]
-
-//     if (!c) {
-//         return ''
-//     } else if (c === 1) {
-//         return '1 pitcher'
-//     } else {
-//     function placeDownstreamGroups(upstreamId, column) {
-//         nodesSorted.forEach(pg => {
-//             var upstreamGroups = (pg.upstreamGroups || '').split(',')
-//             if (upstreamGroups && upstreamGroups.indexOf(upstreamId) > -1) {
-//                 if (!pitcherGroupPlaced[pg.id]) {
-//                     placeGroupInColumn(pg.id, column)
-//                     pitcherGroupPlaced[pg.id] = true
-//                     placeDownstreamGroups(pg.id, column + 1)
-//                 } else if (nodesSorted[pg.id].x > column) {
-//                     freeUpRow(
-//                         indexes,
-//                         nodesSorted[pg.id].x,
-//                         nodesSorted[pg.id].y,
-//                     )
-//                     delete nodesSorted[pg.id]
-//                     placeGroupInColumn(pg.id, column)
-//                     pitcherGroupPlaced[pg.id] = true
-//                     placeDownstreamGroups(pg.id, column + 1)
-//                 }
-//                 connectors.push({ from: upstreamId, to: pg.id })
-//             }
-//         })
-//     }
-
-//     function placeGroupInColumn(pgid, column) {
-//         if (!nodesSorted[pgid]) {
-//             nodesSorted[pgid] = {
-//                 x: column,
-//                 y: firstFreeRow(indexes, column),
-//                 purpose: pitcherCountString(pgid),
-//                 name: pgid,
-//                 class: 'downdown',
-//             }
-//         }
-//     }
-
-//     function pitcherCountString(pgid) {
-//         var c = pitcherCount[pgid]
-
-//         if (!c) {
-//             return ''
-//         } else if (c === 1) {
-//             return '1 pitcher'
-//         } else {
-//             return c + ' pitchers'
-//         }
-//     }
-
-//     // Return true if the pitcher-group has at least
-//     // one upstream group that exists.
-//     function hasValidUpstream(pg) {
-//         let valid = false
-//         const upstream = (pg.upstreamGroups || '').split(',')
-//         upstream.forEach(upstream => {
-//             if (pitcherGroup[upstream]) {
-//                 valid = true
-//             }
-//         })
-//         return valid
-//     }
-
-//     var pitcherCount = {}
-//     pitchers.forEach(p => {
-//         if (!pitcherCount[p.pitcherGroup]) {
-//             pitcherCount[p.pitcherGroup] = 1
-//         } else {
-//             pitcherCount[p.pitcherGroup]++
-//         }
-//     })
-
-//     nodesSorted.forEach(pgi => {
-//         if (pgi.ingress === 'yes' || pgi.coreIngress === 'yes') {
-//             placeGroupInColumn(pgi.id, 0)
-//             placeDownstreamGroups(pgi.id, 1)
-//         } else if (!hasValidUpstream(pgi)) {
-//             placeGroupInColumn(pgi.id, 1)
-//             placeDownstreamGroups(pgi.id, 2)
-//         }
-//     })
-//     return { nodes: nodesArray(nodesSorted), connectors: connectors }
-// }
-
-// function firstFreeRow(indexes, column) {
-//     if (indexes[column]) {
-//         indexes[column]++
-//     } else {
-//         indexes[column] = 1
-//     }
-//     return indexes[column]
-// }
-
-// function freeUpRow(indexes, column, row) {
-//     if (indexes[column] === row) {
-//         indexes[column]--
-//     }
-// }
-
-// function nodesArray(nodes) {
-//     let result = []
-//     for (let nodeId of Object.keys(nodes)) {
-//         const node = nodes[nodeId]
-//         const row = result[node.y] || []
-//         row[node.x] = { id: nodeId }
-//         result[node.y] = row
-//     }
-//     return result.filter(row => !!row)
-// }
 
 export function setCell(
     matrix: Matrix,
@@ -230,5 +109,15 @@ export function findFreeCell(
     row: number,
     column: number,
 ): number {
-    return 1
+    // Start search from last row
+    let bestyet = matrix.length
+    while (
+        bestyet > row &&
+        matrix[bestyet - 1]
+            .slice(column)
+            .reduce((acc, node) => acc && !node, true)
+    ) {
+        bestyet--
+    }
+    return bestyet
 }
--- test/test-basic.ts
+++ test/test-basic.ts
@@ -1,5 +1,5 @@
 import * as test from 'purple-tape'
-import { gridGraphPlacement, setCell } from '../index'
+import { gridGraphPlacement, setCell, findFreeCell, buildGraph } from '../index'
 import {
     parseConnectors,
     nodesFromConnectors,
@@ -7,13 +7,6 @@ import {
     stringsMatrix,
 } from '../src/util'
 import { Matrix } from '../src/types'
-
-test('gridGraphPlacement', t => {
-    const connectors = parseConnectors('A => B => C')
-    const nodes = nodesFromConnectors(connectors)
-    const gg = gridGraphPlacement(nodes, connectors)
-    t.equal(matrixStrings(gg), 'ABC')
-})
 
 test('setCell empty', t => {
     const matrix: Matrix = []
@@ -25,4 +18,42 @@ test('setCell add one', t => {
     const matrix = stringsMatrix('AB\nC_')
     setCell(matrix, 1, 1, { id: 'D' })
     t.equal(matrixStrings(matrix), 'AB\nCD')
+})
+
+test('findFreeCell', t => {
+    const matrix = stringsMatrix('AB')
+    t.equal(findFreeCell(matrix, 0, 2), 0)
+    t.equal(findFreeCell(matrix, 0, 1), 1)
+})
+
+test('A => B => C', t => {
+    const connectors = parseConnectors('A => B => C')
+    const nodes = nodesFromConnectors(connectors)
+    const matrix = buildGraph(nodes, connectors)
+    t.equal(matrixStrings(matrix), 'ABC')
+})
+
+test('A => B => C B => D', t => {
+    const connectors = parseConnectors('A => B => C B => D')
+    const nodes = nodesFromConnectors(connectors)
+    const matrix = buildGraph(nodes, connectors)
+    t.equal(matrixStrings(matrix), 'ABC\n__D')
+})
+
+test('A => B => C B => D E => F', t => {
+    const connectors = parseConnectors('A => B => C B => D E => F')
+    const nodes = nodesFromConnectors(connectors)
+    const matrix = buildGraph(nodes, connectors)
+    t.equal(matrixStrings(matrix), 'ABC\n__D\nEF_')
+})
+
+// B and E connected in both directions.
+// Place E directly below E
+test('A => B => C B => D B => E E => B E => F', t => {
+    const connectors = parseConnectors(
+        'A => B => C B => D B => E E => B E => F',
+    )
+    const nodes = nodesFromConnectors(connectors)
+    const matrix = buildGraph(nodes, connectors)
+    t.equal(matrixStrings(matrix), 'ABC\n__D\n_EF')
 })
